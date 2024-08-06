5ba646a264ebfae93f5a7c69a49501b193c391d2
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-08-07 02:15:06 +0530

    database connected sucessfully

---
+++ db_connector.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+import mysql.connector
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Connect to the database using environment variables
+mydb = mysql.connector.connect(
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    database=os.getenv('DB_NAME')
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM books")
+
+for i in mycursor:
+    print(i)
